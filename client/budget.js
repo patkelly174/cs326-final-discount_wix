1c1d9d8852740d598c73c68e8fa6d038aa784a01
yonsiojnag <yonsiojnag@gmail.com>
2022-05-05 18:35:30 -0400

    fixed removeBudget

--- client/budget.js
+++ client/budget.js
@@ -15,7 +15,7 @@ function addMoney() {
 function removeMoney() {
     let totalBudget = parseInt(document.getElementById("totalBudget").innerHTML);
     let currentBalance = parseInt(document.getElementById("currentBalance").innerHTML);
-    let money = parseInt(document.getElementById("money").value);
+    let money = parseInt(document.getElementById("money_1").value);
     if(money === "" || isNaN(money)) {
         alert("Please add money");
     } else {
@@ -28,8 +28,8 @@ function removeMoney() {
 
 function addExpense() {
     let currentBalance = parseInt(document.getElementById("currentBalance").innerHTML);
-    let expenseName = document.getElementById("expenseName").value;
-    let expenseAmount = parseInt(document.getElementById("expenseAmount").value);
+    let expenseName = document.getElementById("expenseName_1").value;
+    let expenseAmount = parseInt(document.getElementById("expenseAmount_1").value);
     if(expenseName.length === "") {
         alert("Please add expense name");
     } else if(expenseAmount === "" || isNaN(expenseAmount)) {
@@ -42,5 +42,24 @@ function addExpense() {
         document.getElementById("addedExpenses").appendChild(para);
     } else {
         alert("Amount is higher than your current balance");
+    }
+}
+
+function removeExpense() {
+    let currentBalance = parseInt(document.getElementById("currentBalance").innerHTML);
+    let expenseName = document.getElementById("expenseName_2").value;
+    let expenseAmount = parseInt(document.getElementById("expenseAmount_2").value);
+    if(expenseName.length === "") {
+        alert("Please add expense name");
+    } else if(expenseAmount === "" || isNaN(expenseAmount)) {
+        alert("Please add valid amount");
+    } else if(expenseAmount < currentBalance) {
+        currentBalance += expenseAmount;
+        document.getElementById("currentBalance").innerHTML = currentBalance;
+        let para = document.createElement('p');
+        para.innerHTML = `Expense Name : ${expenseName} <br> Expense Amount: ${expenseAmount}`;
+        document.getElementById("addedExpenses").appendChild(para);
+    } else {
+        alert("Amount is lower than your current balance");
     }
 }
